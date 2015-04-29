307361ea717d918af77ade93befae4025cf14fd1
Robert Hewitt <robert@kotikan.com>
2015-04-29 23:42:43 +0100

    cancels all previously shown notifications from this app when we have a confirmation confirmed this seems like the safest way to ensure we don't have lingering notifications after we have confirmed something

--- wear/src/main/java/com/kotikan/demo/taxitracker/activities/AlwaysExitsConfirmationActivity.java
+++ wear/src/main/java/com/kotikan/demo/taxitracker/activities/AlwaysExitsConfirmationActivity.java
@@ -1,5 +1,8 @@
 package com.kotikan.demo.taxitracker.activities;
 
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.wearable.activity.ConfirmationActivity;
@@ -17,6 +20,15 @@ public class AlwaysExitsConfirmationActivity extends ConfirmationActivity {
 
         final Handler handler = new Handler();
         handler.postDelayed(killActivity(), 2000);
+
+        final Intent intent = getIntent();
+        final Bundle extras = intent.getExtras();
+        if (extras != null) {
+            if (ConfirmationActivity.SUCCESS_ANIMATION == extras.getInt(ConfirmationActivity.EXTRA_ANIMATION_TYPE)) {
+                ((NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE)).cancelAll();
+            }
+        }
+
         wakeLock.onCreate(this);
     }
 
